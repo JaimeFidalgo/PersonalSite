9dd9820ff1e782afddab2f552afc7645a69ff408
JaimeFidalgo <jaimefidalgosantos@gmail.com>
2022-05-26 18:23:06 +0200

    first commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static(path.resolve(__dirname,'public')))//permitir acceso a los
 //routes
 app.use(require('./back-end/routes/routes')); //importo las rutas
 
-s
+
 
 
 // starting server
